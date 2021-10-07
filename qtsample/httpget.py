42331dd9a7f04e9584e6504b96acd3ae81ae9943
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2021-10-06 19:01:55 -0700

    start using git

---
+++ qtsample/httpget.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+from PySide6 import QtCore, QtWidgets
+from __feature__ import snake_case  # noqa: F401
+from __feature__ import true_property  # noqa: F401
+
+
+class HTTPGet(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.url = QtWidgets.QLineEdit()
+        self.url.placeholder_text = "URL here"
+
+        self.get_button = QtWidgets.QPushButton("Get the page!!!")
+        self.get_button.clicked.connect(self.get)
+
+        self.content = QtWidgets.QTextEdit()
+        self.content.placeholder_text = "Content here"
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.add_widget(self.url)
+        self.layout.add_widget(self.get_button)
+        self.layout.add_widget(self.content)
+
+    @QtCore.Slot()
+    def get(self):
+        self.content.text = requests.get(self.url.text)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = HTTPGet()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
